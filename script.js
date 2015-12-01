141991cb58cc6c2b2ba6e494abfc38ebf669a0ae
NikEurope <nik.europe@gmail.com>
2015-12-01 16:00:16 +0100

    sitetest

---
+++ script.js
@@ -0,0 +1,19 @@
+function byId(){
+return document.getElementById(elementId);
+}
+
+function byClass(){
+return document.getElementsByClasseName(elementClass);
+}
+
+function byTag(){
+return document.getElementsByTagName(elementTag);
+}
+
+function addClass(){
+
+}
+
+function removeClass(){
+
+}
